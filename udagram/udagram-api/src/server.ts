ab6d9ac677fde0b69530a42cd74dbdd6f5e84cf6
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-10-12 12:52:55 +0700

    Fix error when register

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -53,7 +53,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   });
 
   // Start the Server
-  app.listen(port, () => {
+  app.listen(8080, () => {
     console.log(`Backend server is listening on port ${port}....`);
     console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
